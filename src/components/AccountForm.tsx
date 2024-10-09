653ec5ff6b84e5b48ad319e704b4ff70a96c3fc4
dl058 <dinh.leeza058@gmail.com>
2024-10-09 13:55:36 -0400

    login auth

--- src/components/AccountForm.tsx
+++ src/components/AccountForm.tsx
@@ -1,7 +1,61 @@
+import { FormEvent, useState } from "react";
 import "./AccountForm.css";
 
 const AccountForm = () => {
-  return <div className="AccountForm">AccountForm works</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dob, setDOB] = useState("");
+
+  const submitHandler = (e: FormEvent): void => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="AccountForm">
+      <h2>Create New Account</h2>
+      <h3>Already Registered? Login</h3>
+      <form className="new-account-form" onSubmit={submitHandler}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <label htmlFor="dob">Date of Birth</label>
+        <input
+          type="date"
+          name="dob"
+          id="dob"
+          value={dob}
+          onChange={(e) => setDOB(e.target.value)}
+          required
+        />
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
+  );
 };
 
 export default AccountForm;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
 import "./Header.css";
+import { signInWithGoogle, signOutOfGoogle } from "../firebaseConfig";
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
 
 const Header = () => {
-  return <div className="Header">Header works</div>;
+  const { user } = useContext(AuthContext);
+  return (
+    <header className="Header">
+      <Link to="/">
+        <h1>Shoutouts App</h1>
+      </Link>
+      {user === null ? (
+        <button onClick={signInWithGoogle}>Sign In</button>
+      ) : (
+        <>
+          <p>Welcome, {user?.displayName}</p>
+          <img src={user.photoURL || ""} alt={user.displayName || ""} />
+          <button onClick={signOutOfGoogle}>Sign Out</button>
+        </>
+      )}
+    </header>
+  );
 };
 
 export default Header;
