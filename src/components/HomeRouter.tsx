699029d0097ed1411e9cdff62fd2aa7d44da26a7
dl058 <dinh.leeza058@gmail.com>
2024-10-09 10:57:13 -0400

    func

---
+++ src/components/HomeRouter.tsx
@@ -0,0 +1,22 @@
+import "./HomeRouter.css";
+
+const HomeRouter = () => {
+  return (
+    <>
+      <>
+        <h1> unPlanned Adventure </h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. In voluptas
+          excepturi doloribus animi ullam nemo aperiam quod. Quam laboriosam,
+          molestiae, velit eius aliquid dolor qui non, error voluptatibus
+          dignissimos fugiat?
+        </p>
+        <button> Log In </button>
+      </>
+      <AccountRouter />
+      <EventRouter />
+    </>
+  );
+};
+
+export default HomeRouter;
