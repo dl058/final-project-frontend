a065e101bd9ba68b7af035afd1623b2ebe4fd5f5
David Duong <duongdavid17@gmail.com>
2024-10-09 15:37:41 -0400

    dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,26 @@
+import { useLayoutEffect, useState } from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div className="Header">Header works</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropDown = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <header className="header">
+      <img
+        src="src/assets/threeLines.png"
+        alt="lines"
+        onClick={toggleDropDown}
+      />
+      {isOpen ? (
+        <ul className="dropdown">
+          <li>Preferences</li>
+          <li>Sign Up</li>
+        </ul>
+      ) : null}
+    </header>
+  );
 };
 
 export default Header;
