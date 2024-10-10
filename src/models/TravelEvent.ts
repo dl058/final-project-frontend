5ec6fb33e3651aa33144c7cd2810498b50d0ef1e
dl058 <dinh.leeza058@gmail.com>
2024-10-10 15:06:33 -0400

    more card

--- src/models/TravelEvent.ts
+++ src/models/TravelEvent.ts
@@ -7,16 +7,54 @@ interface Image {
   url: string;
 }
 
+interface Dates {
+  start: Start;
+}
+
+interface Start {
+  localDate: string;
+  localTime: string;
+  dateTime: string;
+}
+
+interface PriceRange {
+  currency: string;
+  min: number;
+  max: number;
+}
+
+interface Venues {
+  postalCode: string;
+  timezone: string;
+  city: City;
+  state: State;
+}
+
+interface City {
+  name: string;
+}
+
+interface State {
+  name: string;
+  stateCode: string;
+}
+
+interface Embedded {
+  venues: Venues[];
+}
+
 export default interface TravelEvent {
   name: string;
   type: string;
   id: string;
   url: string;
-  locale: string;
-
   images: Image[];
   geoPoint?: GeoPoint;
-  venueId?: string;
-  attractionId?: string;
+  venue: Venues;
   keyword?: string;
+  dates: Dates;
+  priceRange: PriceRange[];
+  state: State;
+  city: City;
+  _embedded: Embedded;
 }
