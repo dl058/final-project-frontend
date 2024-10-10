0f1b61923b89f6013fecc468b4e06b8942b2fe22
dl058 <dinh.leeza058@gmail.com>
2024-10-10 11:55:00 -0400

    a lot of card stuff

--- src/components/HomeRouter.tsx
+++ src/components/HomeRouter.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import AccountRouter from "./AccountRouter";
-import EventRouter from "./EventRouter";
+
 import "./HomeRouter.css";
 import { getEventNearMe, getRandomEvents } from "../services/eventService";
 import TravelEvent from "../models/TravelEvent";
+import EventList from "./EventList";
 
 const HomeRouter = () => {
   const [events, setEvents] = useState<TravelEvent[] | null>(null);
@@ -17,13 +18,13 @@ const HomeRouter = () => {
       geopoint.latitude = position.coords.latitude;
       geopoint.longitude = position.coords.longitude;
       getEventNearMe(geopoint).then((res) => {
-        setEvents(res._embedded);
+        setEvents(res._embedded.events);
       });
     };
     const failure = () => {
       console.log("Geolocation not available");
       getRandomEvents().then((res) => {
-        setEvents(res._embedded);
+        setEvents(res._embedded.events);
       });
     };
     if (navigator.geolocation) {
@@ -44,7 +45,7 @@ const HomeRouter = () => {
         </p>
       </>
       <AccountRouter />
-      <EventRouter />
+      <EventList travelEvents={events} />
     </>
   );
 };
