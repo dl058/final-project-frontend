2045edbc5ea57243acf7288d4011bc540c38abf6
Leeza Dinh <dinh.leeza058@gmail.com>
2024-10-17 11:06:39 -0400

    fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,52 +21,52 @@ const Header = () => {
           u<span className="highlighted">nP</span>lan'd Adventure{" "}
         </h1>
       </Link>
-      {isOpen ? (
-        <ul className="dropdown">
-          <li>
-            <Link to="/">
-              <i className="fa-solid fa-house"></i> Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/events">
-              <i className="fa-solid fa-calendar"></i> Events
-            </Link>
-          </li>
-          <li>
-            <Link to="search">Search</Link>
-          </li>
-          <li>
-            <Link to="/favorites">
-              <i className="fa-solid fa-heart"></i> Favorites
-            </Link>
-          </li>
-          <li>
-            <Link to="search">
-              <i className="fa-solid fa-magnifying-glass"></i> Search
-            </Link>
-          </li>
-          <li>
-            {" "}
-            {user === null ? (
-              <button onClick={signInWithGoogle} className="signBtn">
-                Sign In
-              </button>
-            ) : (
-              <>
-                <button onClick={signOutOfGoogle} className="signBtn">
-                  Sign Out
+      <div className="navBox">
+        {isOpen ? (
+          <ul className="dropdown">
+            <li>
+              <Link to="/">
+                <i className="fa-solid fa-house"></i> Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/events">
+                <i className="fa-solid fa-calendar"></i> Events
+              </Link>
+            </li>
+            <li>
+              <Link to="search">
+                <i className="fa-solid fa-magnifying-glass"></i> Search
+              </Link>
+            </li>
+            <li>
+              <Link to="/favorites">
+                <i className="fa-solid fa-heart"></i> Favorites
+              </Link>
+            </li>
+
+            <li>
+              {" "}
+              {user === null ? (
+                <button onClick={signInWithGoogle} className="signBtn">
+                  Sign In
                 </button>
-              </>
-            )}
-          </li>
-        </ul>
-      ) : null}
-      {user !== null && (
-        <>
-          <p>Welcome, {user?.displayName}</p>
-        </>
-      )}
+              ) : (
+                <>
+                  <button onClick={signOutOfGoogle} className="signBtn">
+                    Sign Out
+                  </button>
+                </>
+              )}
+            </li>
+          </ul>
+        ) : null}
+        {user !== null && (
+          <>
+            <p>Welcome, {user?.displayName}</p>
+          </>
+        )}
+      </div>
       <i className="fa-solid fa-bars" onClick={toggleDropDown}></i>
     </header>
   );
